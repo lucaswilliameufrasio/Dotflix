2e8027bc183d7ffb428a3ad27519a67a24df7160
Lucas William <lucaswilliameufrasio@gmail.com>
2020-08-01 05:45:07 -0300

    fix: import components correctly

--- web/src/routes.js
+++ web/src/routes.js
@@ -5,21 +5,15 @@ import {
   Route,
   Redirect
 } from 'react-router-dom'
-import App from './App'
 
-function CadastroVideo() {
-  return (
-    <div>
-      <h1>CadastroVideo</h1>
-    </div>
-  )
-}
+import Home from './pages/Home'
+import CadastroVideo from './pages/CadastroVideo'
 
 function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={App} />
+        <Route exact path="/" component={Home} />
         <Route path="/cadastro/video" component={CadastroVideo} />
 
         <Route path="*" exact render={() => <Redirect to="/" />} />
