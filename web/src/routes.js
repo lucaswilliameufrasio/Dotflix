14e0cc18747d48d0cf424bb41b9d479829c318a2
Lucas William <lucaswilliameufrasio@gmail.com>
2020-08-03 07:53:56 -0300

    style: code formatting

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 import Home from './pages/Home'
 import CadastroVideo from './pages/CadastroVideo'
